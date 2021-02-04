a69d042b1d7633ae7685e276449e30165a2fd0d2
Gurveen <gurveens11@gmail.com>
2021-02-04 11:45:48 +0530

    Create Board component

---
+++ src/components/Game.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { calculateWinner } from "../helper";
+import Board from "./Board";
+
+const Game = () => {
+  const [history, setHistory] = useState(Array(9).fill(null));
+  return <div></div>;
+};
+
+export default Game;
